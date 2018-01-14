a551704dec69637e0759d9f74ed5f5da185d6189
Vigan Sokoli <arietamehmeti@gmail.com>
2018-01-14 23:31:29 +0100

    The database was emptied and added

--- ddwrt.sql
+++ ddwrt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2018 at 11:25 PM
+-- Generation Time: Jan 14, 2018 at 11:30 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.0.22
 
@@ -25,25 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `main_router`
---
-
-CREATE TABLE `main_router` (
-  `id` int(4) NOT NULL,
-  `in_use` tinyint(1) NOT NULL,
-  `ip` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `main_router`
---
-
-INSERT INTO `main_router` (`id`, `in_use`, `ip`) VALUES
-(3, 1, '192.168.0.1');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `router`
 --
 
@@ -54,22 +35,8 @@ CREATE TABLE `router` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `router`
---
-
-INSERT INTO `router` (`id`, `main_router_id`, `ip`) VALUES
-(5, 3, '192.168.0.3'),
-(6, 3, '192.168.0.4');
-
---
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `main_router`
---
-ALTER TABLE `main_router`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `router`
@@ -81,12 +48,6 @@ ALTER TABLE `router`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `main_router`
---
-ALTER TABLE `main_router`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `router`
